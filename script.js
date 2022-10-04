d182966af4bcf6d9cd1d1d1e16975184eaf2d3e4
sachin singh <sachinchodu001@gmail.com>
2022-10-04 22:36:13 +0530

    uncommented code and modified

--- script.js
+++ script.js
@@ -1,27 +1,27 @@
 /*****************************
 * Variables and data types
 */
-/*
-var firstName = 'John';
+
+var firstName = 'Jonas';
 console.log(firstName);
 
 var lastName = 'Smith';
-var age = 28;
+var age = 28+1;
 
-var fullAge = true;
+var fullAge = !true;
 console.log(fullAge);
 
 var job;
 console.log(job);
 
-job = 'Teacher';
+job = 'best Teacher ';
 console.log(job);
 
 // Variable naming rules
 var _3years = 3;
 var johnMark = 'John and MArk';
 var if = 23;
-*/
+
 
 
 
